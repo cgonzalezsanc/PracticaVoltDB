4145893e92c42dbcdd940408a4064298caf86f50
cgonzalezsanc <carlosgosa94@gmail.com>
2016-11-17 17:18:22 +0100

    Añadido procedimiento FIndAlert, LoadAlert y GetAlertByLocation al towns.sql

--- towns.sql
+++ towns.sql
@@ -44,3 +44,18 @@ CREATE TABLE nws_event (
 
 CREATE INDEX nws_event_idx ON nws_event (id);
 
+CREATE PROCEDURE FindAlert AS
+   SELECT id, updated FROM nws_event
+   WHERE id = ?;
+
+CREATE PROCEDURE FROM CLASS LoadAlert;
+
+CREATE PROCEDURE GetAlertsByLocation 
+   PARTITION ON TABLE local_event COLUMN state_num
+   AS SELECT w.id, w.summary, w.type, w.severity,
+             w.starttime, w.endtime 
+             FROM nws_event as w, local_event as l
+             WHERE l.id=w.id and 
+                   l.state_num=? and l.county_num = ? and
+                   w.endtime > TO_TIMESTAMP(MILLISECOND,?)
+             ORDER BY w.endtime;
